1404c1eb334dde92c71e94725caae6528cca2b2b
makitagithub <yukizuki0204@yahoo.co.jp>
2024-08-24 01:23:40 +0900

    testtest1

--- TESTTEST/Add.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int a = scanf("%d", &a);
-    int b = scanf("%d", &b);
-    int c = a + b;
-    printf("%d", c);
-    return 0;
-}
--- TESTTEST/MulTen.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a = scanf("%d", &a);
-    int b = 10;
-    int c = a * b;
-    printf("%d", c);
-    return 0;
-}
