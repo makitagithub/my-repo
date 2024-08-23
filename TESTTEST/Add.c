b6056cccb1e31478f630b3ae8eb6cedf63098d1b
makitagithub <yukizuki0204@yahoo.co.jp>
2024-08-24 00:26:38 +0900

    testtest

---
+++ TESTTEST/Add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a = scanf("%d", &a);
+    int b = scanf("%d", &b);
+    int c = a + b;
+    printf("%d", c);
+    return 0;
+}
---
+++ TESTTEST/SubFour.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a = scanf("%d", &a);
+    int b = 4;
+    int c = a - b;
+    printf("%d", c);
+    return 0;
+}
